6f5f68e625a26d819b52dfeda52bd385ee1ce22b
Alan Sanchez <alanoswalt01@gmail.com>
2021-01-04 21:17:57 -0600

    Create prototype class for App

--- Password.py
+++ Password.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+
+
+#Class that has the attributes of the account.
+class account():
+    def __init__(self, name, email_username, password):
+        self.name = name
+        self.email_username = email_username
+        self.password = password
+    def __str__(self):
+        return "This is the info of a " + self.name
+
+correo = account("Outlook", "alan@outlook.com", "pepeeltoro")
+
+print(correo)
