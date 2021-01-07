4780f016ce631f5079f18d0a1c8080f4d943eb56
Alan Sanchez <alanoswalt01@gmail.com>
2021-01-06 19:51:27 -0600

    Main function was added Inpus were added Return of the __str__ function inside account class was modify.

--- Password.py
+++ Password.py
@@ -1,5 +1,3 @@
-
-
 #Class that has the attributes of the account.
 class account():
     def __init__(self, name, email_username, password):
@@ -7,8 +5,21 @@ class account():
         self.email_username = email_username
         self.password = password
     def __str__(self):
-        return "This is the info of a " + self.name
+        #return "This is the info of a " + self.name + " with the user " +self.email_username +" and password "+ self.password
+        return "This is the info of a {} with the user {} and password {}".format(self.name, self.email_username, self.password)
 
-correo = account("Outlook", "alan@outlook.com", "pepeeltoro")
 
-print(correo)
+
+def create(name, email, password):
+    new_account = account(name, email, password)
+    print(new_account)
+    return None
+
+def main():
+    x = input('Dame el tipo de cuenta (Gmail, Outlook, Netflix): ')
+    y = input('Dame el correo o usuario: ')
+    z = input('Dame la contraseña: ')
+    create(x, y, z)
+
+if __name__=="__main__":
+    main()
