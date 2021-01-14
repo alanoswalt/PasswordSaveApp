c878e975b51059bc2fd27802a7d43d37465819c6
Alan Sanchez <alanoswalt01@gmail.com>
2021-01-13 20:56:56 -0600

    Add new code lines in load_file for the Data Base

--- Password.py
+++ Password.py
@@ -1,4 +1,6 @@
 #Class that has the attributes of the account.
+import sqlite3
+
 class account():
     def __init__(self, name, email_username, password):
         self.name = name
@@ -10,16 +12,38 @@ class account():
 
 
 
+def load_file(new_account):
+    conn = sqlite3.connect('accountsdb.sqlite')
+    cur = conn.cursor()
+    cur.executescript('''
+    CREATE TABLE IF NOT EXITS Accounts (
+        type   TEXT     ,
+        mail_user   TEXT,
+        password TEXT
+    ); ''')
+
+    cur.execute('''INSERT OR IGNORE INTO Accounts (type)
+        VALUES ( ? )''', ( new_account.name, ) )
+
+    cur.execute('''INSERT OR IGNORE INTO Accounts (mail_user)
+        VALUES ( ? )''', ( new_account.email_username, ) )
+
+    cur.execute('''INSERT OR IGNORE INTO Accounts (password)
+        VALUES ( ? )''', ( new_account.password, ) )
+    return None
+
 def create(name, email, password):
     new_account = account(name, email, password)
     print(new_account)
-    return None
+    return new_account
 
 def main():
     x = input('Dame el tipo de cuenta (Gmail, Outlook, Netflix): ')
     y = input('Dame el correo o usuario: ')
     z = input('Dame la contraseña: ')
-    create(x, y, z)
+    account = create(x, y, z)
+    load_file(accout)
+
 
 if __name__=="__main__":
     main()
